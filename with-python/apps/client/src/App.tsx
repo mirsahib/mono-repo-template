bd6535a6e5c4bda737821b66941c6012721b8256
Turbobot <turbobot@vercel.com>
2025-02-24 08:21:43 +0600

    feat(create-turbo): apply official-starter transform

--- with-python/apps/client/src/App.tsx
+++ with-python/apps/client/src/App.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Button } from '@repo/ui/button'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -37,6 +38,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <Button appName='Python'>Click me</Button>
     </>
   )
 }
